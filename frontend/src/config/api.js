504a2fd8678c703623487cfa8d9ca6b30e564588
Monika Dewangan <61625011+monika163@users.noreply.github.com>
2024-07-28 12:17:48 +0530

    Update api.js
    44rgr4g5g

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const DEPLOYED = "https://reference-coding.onrender.com"
 const LOCALHOST = "http://localhost:8080";
 
-export const API_BASE_URL = LOCALHOST;
+export const API_BASE_URL = DEPLOYED;
 
 const api = axios.create({
   baseURL: API_BASE_URL
